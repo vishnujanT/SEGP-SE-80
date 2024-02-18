2ec3bc167be5abb515253fb5a2840242e92ec4b3
Keyuran26 <158284509+Keyuran26@users.noreply.github.com>
2024-02-18 23:46:15 +0530

    Add files via upload

---
+++ ContactUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ContactUs = () => {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>Feel free to get in touch with us using the following contact information:</p>
+      <ul>
+        <li>Email: contact@example.com</li>
+        <li>Phone: +1234567890</li>
+        <li>Address: 123 Main Street, City, Country</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ContactUs;
